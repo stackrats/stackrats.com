0bf8afb5773af4fd1967ead62fb7e4fab6832ace
Stackrats <matt@stackrats.com>
2025-11-28 12:17:13 +0700

    fix typo

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -204,7 +204,7 @@ const formatPercentage = (value: number) => {
             <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
                 <Card>
                     <CardHeader>
-                        <CardTitle>Revenue by Year</CardTitle>
+                        <CardTitle>Revenue by year</CardTitle>
                     </CardHeader>
                     <CardContent>
                         <div class="h-[300px]">
